b434be1587c67cb6f5bf8d11fa57aaacb8ff8a47
Jain, Milan <milan.jain@pnnl.gov>
2021-06-02 13:43:26 -0700

    restructuring the code

--- proxy_apps/utils/data/grid.py
+++ proxy_apps/utils/data/grid.py
@@ -33,9 +33,9 @@ num_edge_feats -- number of features (states) associates with each edge in the n
 
 import scipy.io as sio
 import os
+import time
 import numpy as np
 import pandas as pd
-
 
 class CyberPhysicalDataset:
     def __init__(self, root, gen_locations):
@@ -229,3 +229,123 @@ class SystemData:
         self.num_edges = num_edges
         self.edge_indx = line_data[:, 0:2].T
 # ---------------------------------------------------------------
+
+def load_grid_data(scenario_dir)
+	# input directory
+	print('[INFO]: Loading the datasets from the directory:', scenario_dir)
+	dir_list = os.listdir(scenario_dir)
+	
+	# Indicate the scenario range
+	Dataset = dict()
+	print('[INFO]: Loading data for %d scenarios ...' % len(dir_list))
+
+	l_start = time.time()
+	scenario_data = []
+	count = 0
+	for s_dir in dir_list:
+	    if s_dir.find('scenario') == -1:
+	        continue
+	    dataset = TransientDataset('%s/%s/' % (scenario_dir, s_dir))
+	    original_shape = np.concatenate((dataset.F, dataset.Vm), axis=1).shape
+	    scenario_data.append(np.concatenate((dataset.F[:1000,:], dataset.Vm[:1000,:]), axis=1))
+	    count += 1
+	    if count % 50 == 0:
+	        print('[INFO]: Loaded %d/%d scenarios ...' % (count, len(dir_list)))
+	l_stop = time.time()
+	print('[INFO]: Time taken for loading datasets:', l_stop - l_start, 'seconds')
+	print('[INFO]: Total number of scenarios loaded:', len(scenario_data))
+	print('[INFO]: Shape of each scenario original: ', original_shape)
+	print('[INFO]: Shape of each scenario loaded: ', scenario_data[0].shape)
+	print('[INFO]: Done ...')
+
+	return scenario_data, l_stop-l_start
+
+def create_windows(scenario_data, ds=1, M=2, N=3, window_size=800, shift_size=10):
+	'''
+		M, N: number of time shifts
+		window_size: Length of moving window
+		shift_size: Separation between two moving windows
+	'''
+	i_start = time.time()
+	X_data = [] # Original data
+	Y_data = [] # 1 time-shifted data
+	U_data = [] # 2 time-shifted data
+	V_data = [] # 3 time-shifted data
+	whole_data = [] # Complete data 
+	Yp = [] # For analytical calculations
+	Yf = [] # For analytical calculations
+	count  = 0
+	
+	for dataset in scenario_data:    
+	    dataset_size = dataset.shape[0]
+	    whole_data.append(dataset)      
+	    Yp.append(dataset[:-1,:])
+	    Yf.append(dataset[1:,:])
+	    count += 1
+	    if count % 50 == 0:
+	        print('Done processing %d/%d datasets ...' % (count, len(scenario_data)))
+	    
+	    i = 0
+	    while (i*shift_size+window_size+M+N) <= dataset_size:
+	        X_indices = range(i*shift_size, i*shift_size + window_size,ds)        
+	        Y_indices = range(i*shift_size+1, i*shift_size + window_size+1,ds)               
+	        U_indices = range(i*shift_size+M, i*shift_size + window_size+M,ds)               
+	        V_indices = range(i*shift_size+N, i*shift_size + window_size+N,ds)   
+	        if count < 0:
+	            print(X_indices)
+	            print(Y_indices)
+	            print(U_indices)
+	            print(V_indices)        
+
+	        i = i + 1
+	        X_data.append(dataset[X_indices])
+	        Y_data.append(dataset[Y_indices])
+	        U_data.append(dataset[U_indices])
+	        V_data.append(dataset[V_indices])
+
+	i_stop = time.time()
+	print('[INFO]: Time taken for creating X datasets:', i_stop - i_start, 'seconds')
+	print('[INFO]: Original dataset size:', dataset_size)
+	print('[INFO]: Chosen dataset size:', window_size)
+	print('[INFO]: Length of X_data: ', len(X_data))
+	print('[INFO]: Length of each window after down sampling: ', X_data[0].shape)
+
+	return X_data, Y_data, U_data, V_data, whole_data, Yp, Yf, i_stop - i_start
+
+def scale_data(X_data, Y_data, U_data, V_data, whole_data, Yp, Yf, data_len, scale_factor = 2*np.pi , norm=True):
+	n_start = time.time()
+
+	X_array = np.asarray(X_data).transpose(2,0,1).reshape(data_len,-1).transpose()
+	Y_array = np.asarray(Y_data).transpose(2,0,1).reshape(data_len,-1).transpose()
+	U_array = np.asarray(U_data).transpose(2,0,1).reshape(data_len,-1).transpose()
+	V_array = np.asarray(V_data).transpose(2,0,1).reshape(data_len,-1).transpose()
+	whole_data_array = np.asarray(whole_data).transpose(2,0,1).reshape(data_len,-1).transpose()
+	Yp_array = np.asarray(Yp).transpose(2,0,1).reshape(data_len,-1).transpose()
+	Yf_array = np.asarray(Yf).transpose(2,0,1).reshape(data_len,-1).transpose()
+	print("[INFO]: whole_data_array shape:", whole_data_array.shape)
+	print('[INFO]: Yp_array shape: ', Yp_array.shape)
+	print('[INFO]: Yf_array shape: ', Yf_array.shape)
+	print('[INFO]: X_array shape: ', X_array.shape)
+	print('[INFO]: Y_array shape: ', Y_array.shape)
+	print('[INFO]: U_array shape: ', U_array.shape)
+	print('[INFO]: V_array shape: ', V_array.shape)
+
+	if norm:  
+	    X_array_old  = X_array
+	    Y_array_old  = Y_array
+	    U_array_old  = U_array
+	    V_array_old  = V_array
+	    Yp_array_old = Yp_array
+	    Yf_array_old = Yf_array
+	    X_array      = np.concatenate((scale_factor*(X_array_old[:,:68] - 60), 10*(X_array_old[:,68:] - 1)), axis = 1) 
+	    Y_array      = np.concatenate((scale_factor*(Y_array_old[:,:68] - 60), 10*(Y_array_old[:,68:] - 1)), axis = 1) 
+	    U_array      = np.concatenate((scale_factor*(U_array_old[:,:68] - 60), 10*(U_array_old[:,68:] - 1)), axis = 1) 
+	    V_array      = np.concatenate((scale_factor*(V_array_old[:,:68] - 60), 10*(V_array_old[:,68:] - 1)), axis = 1) 
+	    Yp_array     = np.concatenate((scale_factor*(Yp_array_old[:,:68] - 60), 10*(Yp_array_old[:,68:] - 1)), axis = 1)
+	    Yf_array     = np.concatenate((scale_factor*(Yf_array_old[:,:68] - 60), 10*(Yf_array_old[:,68:] - 1)), axis = 1)    
+	        
+	n_stop = time.time()
+	print('[INFO]: Time taken for normalization:', n_stop - n_start, 'seconds')
+
+	return X_array, Y_array, U_array, V_array, Yp_array, Yf_array, n_stop-n_start
+
