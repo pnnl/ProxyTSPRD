f7da80ed4a000bc68563b25079f37f9384feaec0
Milan Jain <milan.jain@pnnl.gov>
2021-11-19 11:25:56 -0800

    code cleaning

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-# ML Proxy Applications
-=======
-# TSPRD
->>>>>>> 0835b7595c7cdb156bdb8b22d8be960bc82beea2
+ProxyTSPRD
--- examples/test.sh
+++ examples/test.sh
@@ -8,8 +8,8 @@
 #SBATCH --exclusive
 #SBATCH --constraint=nvlink
 #SBATCH -J horovod
-#SBATCH -o out.txt
-#SBATCH -e err.txt
+#SBATCH -o test_logs/out.txt
+#SBATCH -e test_logs/err.txt
 
 module purge
 module load python/miniconda3.9
