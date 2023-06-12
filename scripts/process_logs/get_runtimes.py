fc46a7f501923f5c9c5bc1ca0721247e12619873
Milan Jain <milan.jain@pnnl.gov>
2023-06-12 10:53:43 -0700

    moving plots out

--- scripts/process_logs/get_runtimes.py
+++ scripts/process_logs/get_runtimes.py
@@ -5,36 +5,44 @@ import json
 import numpy as np
 import pandas as pd
 
+data_dir_name = "onnx"
 # _DATA_DIR = '/home/milanjain91/results/tpdps23/logs/sbatch/'
-_DATA_DIR = '/people/jain432/pacer_remote/output/logs/theta/sbatch/infer/b1/'
+_DATA_DIR = '/people/jain432/pacer_remote/output/iiswc23/logs/' + data_dir_name + '/'
 
 # _OUTPUT_DIR = '/home/milanjain91/results/tpdps23/'
-_OUTPUT_DIR = '/people/jain432/pacer_remote/output/results/'
+_OUTPUT_DIR = '/people/jain432/pacer_remote/output/iiswc23/'
 
 keyword = "infer"
 if keyword == "train_tf":
-    pattern = "o*" + keyword.split("_")[1] + "*e50*prof0*" + keyword.split("_")[0] + "*"
+    patterns = "o*" + keyword.split("_")[1] + "*e50*prof0*" + keyword.split("_")[0] + "*"
     test_str = "\[INFO\] Training on *"
     match_timestr = "============> \(After\) Model Fitting*"
     match_lossstr = ""
 if keyword == "train_pt":
-    pattern = "o*" + keyword.split("_")[1] + "*e50*prof0*" + keyword.split("_")[0] + "*"
+    patterns = "o*" + keyword.split("_")[1] + "*e50*prof0*" + keyword.split("_")[0] + "*"
     test_str = "\[INFO\] Training on *"
     match_timestr = "============> \(After\) Model Fitting*"
     match_lossstr = ""
 elif keyword == "infer":
-    pattern = "o*e1_b1*prof0*" + keyword + "*"
+    if data_dir_name == "onnx":
+        patterns = ["o_*ng1_*prof0*onnx"]
+    else:
+        patterns = ["o_*stgcn*ng1_*e1_*mgpuNone*prof0*" + keyword + "*", "o_*ng1_*e50_*mgpuHVD*prof0*" + keyword + "*"]
     test_str = "\[INFO\] Testing on *"
-    match_timestr = "============> \(After\) Inference Time*"
-    match_lossstr = "============> \(After\) (Inference|Test) Loss*"
+    # match_timestr = "============> \(After\) Inference Time*"
+    # match_lossstr = "============> \(After\) (Inference|Test) Loss*"
+    match_timestr = ".*After.*Inference Time.*"
+    match_lossstr = ".*After.*(Inference|Test).Loss.*"
 
-data_files = glob.glob(_DATA_DIR + pattern)
-data_files.sort()
-
+data_files = []
+for p in patterns:
+    sel_files = glob.glob(_DATA_DIR + p)
+    sel_files.sort()
+    data_files += sel_files
 # print(data_files)
 
 if keyword.split("_")[0] == "train":
-    pattern = "^o_(climate|grid)(lstm|cnn)(tf|pt)_*"
+    pattern = "^o_(climate|grid)(lstm|cnn|stgcng)(tf|pt)_*"
     data_files = [f for f in data_files if re.match(pattern, os.path.basename(f))]
 
 # for f in data_files:
@@ -43,21 +51,21 @@ print("[INFO] Number of data files:", len(data_files))
 # sys.exit(1)
 
 def find_runtime(filename):
-    print(filename)
     run_time = -1
     line_buffer = [None, None, None, None, None]
+    loss = None
     with open(filename, 'r') as fp:
         lines = fp.readlines()
         for row in lines:
             # get time
             row = row.strip()
-            # print("Row:", row)
             if re.match(test_str, row):
+                # print("Row (1):", row)
                 result = re.findall(r'\d+\/\d+', row)[0].split("/")[0]
                 n_testcases = int(result)
                 # print(row, n_testcases)
             elif re.match(match_timestr, row):
-                # print(row)
+                # print("Row (2):", row)
                 result = re.findall(r'\d+\.\d+', row)
                 run_time = float(result[0])
 
@@ -80,7 +88,7 @@ def find_runtime(filename):
                 # print(row, run_time, line_buffer, loss)
                 # sys.exit(row)
             elif re.match(match_lossstr, row):
-                print(row)
+                # print("Row (3):", row)
                 if match_lossstr == "":
                     pass
                 else:
@@ -96,6 +104,7 @@ def find_runtime(filename):
 
 result = []
 for f in data_files:
+    print(f)
     n, t, l = find_runtime(f)
     if t == -1:
         print("[WARNING] No time found in", f)
@@ -110,4 +119,4 @@ df_out = pd.DataFrame(result, columns=['model', 'mgpu_strategy', 'n_gpus', 'dtyp
 df_out = df_out.sort_values(['model', 'n_gpus', 'dtype', 'mgpu_strategy']).reset_index(drop=True)[['model', 'n_gpus', 'dtype', 'mgpu_strategy', 'n_cases', 'runtime', 'loss']]
 
 print(df_out)
-df_out.to_csv(os.path.join(_OUTPUT_DIR, "runtimes_" + keyword + "_v5.csv"))
+df_out.to_csv(os.path.join(_OUTPUT_DIR, "runtimes_" + data_dir_name + ".csv"))
